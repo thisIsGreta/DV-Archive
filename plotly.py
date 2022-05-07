76ad78771627011888e91ca24bbbd1e15d8f9093
Greta <96105703+thisIsGreta@users.noreply.github.com>
2022-05-07 16:55:22 +0800

    Create plotly.py

---
+++ plotly.py
@@ -0,0 +1,69 @@
+import plotly.express as px
+
+# ----------------------------- Histogram 直方图 📊----------------------------#
+hist = px.histogram(df_monthly, 
+                   x='pct_deaths', 
+                   color='washing_hands',
+                   nbins=30,
+                   opacity=0.6,
+                   barmode='overlay',
+                   histnorm='percent',
+                   marginal='box',  # 在图上方又做一个box chart
+                   )
+ 
+hist.update_layout(xaxis_title='Proportion of Monthly Deaths',
+                   yaxis_title='Count',)
+ 
+hist.show()
+
+
+
+# ----------------------------- Box Chart 箱型图-----------------------------#
+box = px.box(df_monthly, 
+             x='washing_hands', 
+             y='pct_deaths',
+             color='washing_hands', #像labels
+             title='How Have the Stats Changed with Handwashing?')
+ 
+box.update_layout(xaxis_title='Washing Hands?',
+                  yaxis_title='Percentage of Monthly Deaths',)
+ 
+box.show()
+
+
+
+# ----------------------------- Bar Chart 柱状图-----------------------------#
+h_bar = px.bar(x = category_installs.Installs,
+               y = category_installs.index,
+               orientation='h',
+               title='Category Popularity')
+
+h_bar.update_layout(xaxis_title='Number of Downloads', yaxis_title='Category')
+
+h_bar.show()
+
+
+
+# -----------------------------Pie Chart 饼图-------------------------------#
+fig = px.pie(labels=ratings.index,
+             values=ratings.values,
+             title="Content Rating",
+             names=ratings.index,
+             )
+
+fig.update_traces(textposition='outside', textinfo='percent+label')
+ 
+fig.show()
+
+
+
+# -----------------------------Donut Chart 甜甜圈图-------------------------#
+fig = px.pie(labels=ratings.index,
+             values=ratings.values,
+             title="Content Rating",
+             names=ratings.index,
+             hole = 0.6
+             )
+fig.update_traces(textposition='inside', textinfo='percent')
+ 
+fig.show()
