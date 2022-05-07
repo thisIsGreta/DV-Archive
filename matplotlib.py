08e54da6e8f7b97fed18b7b37635623a5b94e9a6
Greta <96105703+thisIsGreta@users.noreply.github.com>
2022-05-07 17:05:40 +0800

    Update matplotlib.py

--- matplotlib.py
+++ matplotlib.py
@@ -24,6 +24,36 @@ ax1.set_ylim([0, 600])
 ax1.set_xlim([df_tesla.MONTH.min(), df_tesla.MONTH.max()])
 
 
+
+# ----------------------------- Scatter + Rolling 散点趋势图---------------------------------#
+rolling = average.rolling(window=5).mean()
+
+plt.figure(figsize=(16,8), dpi=200)
+plt.title('Number of Nobel Prizes Awarded per Year', fontsize=18)
+
+plt.yticks(fontsize=14)
+plt.xticks(ticks=np.arange(1900, 2021, step=5), 
+           fontsize=14, 
+           rotation=45)
+ 
+ax = plt.gca()    # get current axis
+ax.set_xlim(1900, 2020)
+ 
+ax.scatter(x=over_time_df.index, 
+           y=over_time_df.values, 
+           c='dodgerblue',
+           alpha=0.7,
+           s=100,)
+
+ax.plot(over_time_df.index, 
+        roll_over_time_df.values, 
+        c='crimson', 
+        linewidth=3,)
+ 
+plt.show()
+
+
+
 # --------------------------How to add tick formatting for dates on the x-axis. ---------------------------- #
 ax1.xaxis.set_major_locator(years)
 ax1.xaxis.set_major_formatter(years_fmt)
